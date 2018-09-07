33f256babfbcd897d2b05f6b3d9d9a8c7f44f81c
srajan <srajan.dongre@gmail.com>
2018-09-07 18:32:30 +0530

    integrate search API, title change, remove junk elements from page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,8 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    })
+    }),
+    HttpModule
   ],
   declarations: [
     AppComponent,
