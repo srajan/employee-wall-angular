405580b083d2cbac8fa76dd5c2dcb306e5795dc0
srajan <srajan.dongre@gmail.com>
2018-09-08 23:13:24 +0530

    add hyperlink on names in search result, redirects to user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+	searchResult = '';
+	apiRoot: string = "https://d08sxmexn3.execute-api.ap-south-1.amazonaws.com/dev/employee-wall/";
+	user = '';
 
-  constructor() { }
+	constructor(private activatedRoute: ActivatedRoute, private http: Http) {
+		this.activatedRoute.queryParams.subscribe(params => {
+		    this.user = (typeof params['user']) !== "undefined" ? params['user'] : 'farhat';
+		    console.log(this.user); // Print the parameter to the console. 
 
-  ngOnInit() {
+	    	this.search(this.user);
+		});
+	}
+
+  ngOnInit() { }
+
+  search (userName){
+    let url = `${this.apiRoot}search?searchKey=` + userName;
+    this.http.get(url).subscribe(
+      res => {
+        //console.log(JSON.parse(res._body))
+        if(res['_body']){
+          //console.log(JSON.parse(JSON.parse(res['_body']).body));
+          this.searchResult = JSON.parse(JSON.parse(res['_body']));
+          console.log(this.searchResult);
+        }
+      }); 
   }
 
 }
